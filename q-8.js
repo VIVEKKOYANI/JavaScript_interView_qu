6557d80daadff29da0fc8d1093d4daffb64f1ae7
vivek koyani <vivek.koyani@gmail.com>
2024-06-27 09:45:19 +0530

    PaeseInt Method

---
+++ q-8.js
@@ -0,0 +1,5 @@
+console.log(parseInt("108"));        // Output: 108
+console.log(parseInt("108*200"));    // Output: 108
+console.log(parseInt("108Wonders")); // Output: 108
+
+//! In all cases provided, parseInt() successfully parses "108", ignoring any non-numeric characters that follow.
