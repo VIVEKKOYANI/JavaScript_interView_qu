ffd3df62a006561a3e127af94b8e1f66bf1b028b
vivek koyani <vivek.koyani@gmail.com>
2024-06-27 11:48:18 +0530

    math.random number

---
+++ q-12.js
@@ -0,0 +1,5 @@
+console.log("random", Math.random());
+
+//! The Math.random() static method returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1, with approximately uniform distribution over that range
+
+//! Math.random() does not provide cryptographically secure random numbers. Do not use them for anything related to security. Use the Web Crypto API instead, and more precisely the Crypto.getRandomValues() method.
