21726b8d789b96664c140d6bdbdec06e2d80be05
vivek koyani <vivek.koyani@gmail.com>
2024-06-27 08:46:15 +0530

    string with addition

---
+++ q-2.js
@@ -0,0 +1,5 @@
+let num = 20; // if here is const so they give error
+console.log("string", num + "20");
+console.log("addi", ++num);
+// console.log("addi", +++num); Invalid left-hand side expression
+// console.log("addi", ++num++); Invalid left-hand side expression
