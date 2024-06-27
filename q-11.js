5a5d3ed8d9539b4d318ef781b098616d12b90158
vivek koyani <vivek.koyani@gmail.com>
2024-06-27 11:39:03 +0530

    using check setTimeout global or block scope

---
+++ q-11.js
@@ -0,0 +1,16 @@
+for(var i =0; i < 10; i++){
+    setTimeout(() => {console.log(i)}, 0);
+}
+// outPut
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+
+// var is global scope 
