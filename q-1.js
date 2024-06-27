3e80ca976c9f0bb000818594288ba5151c53a2fb
vivek koyani <vivek.koyani@gmail.com>
2024-06-27 08:26:13 +0530

    diffrent way to define methods with number

---
+++ q-1.js
@@ -0,0 +1,5 @@
+const data = 100;
+console.log("with_method", data.toString());
+// console.log("with_out_method_Error", 100.toString());
+console.log("with_out_method", 100..toString());
+console.log("with_out_method", (100).toString());
